d11e7e7260f32732d328dcdde6cdee06a422c221
Andrew Stewart <andrew@stwrt.ca>
2014-04-01 18:45:21 -0700

    Improve specs for Firmata adaptor

--- test/specs/firmata.spec.js
+++ test/specs/firmata.spec.js
@@ -1,162 +1,353 @@
 'use strict';
 
-source('firmata');
+var LibFirmata = require('firmata');
+
+var adaptor = source('firmata');
 
 describe('Cylon.Adaptors.Firmata', function() {
-  var firmata = new Cylon.Adaptors.Firmata({
-    name: "TestFirmata",
-    connection: "testConnection"
+  var firmata;
+
+  beforeEach(function() {
+    firmata = new adaptor({
+      name: "Firmata",
+      connection: { port: "/dev/ttyACM0", emit: spy() }
+    });
   });
 
-  describe('initialization', function() {
-    it('sets @myself as a reference to the Firmata instance', function() {
+  describe("constructor", function() {
+    it("sets @board to an empty string by default", function() {
+      expect(firmata.board).to.be.eql("");
+    });
+
+    it("sets @myself as a circular reference", function() {
       expect(firmata.myself).to.be.eql(firmata);
     });
 
-    it('sets @connection to the provided connection', function() {
-      expect(firmata.connection).to.be.eql("testConnection");
-    });
-
-    it('sets @name to the provided name', function() {
-      expect(firmata.name).to.be.eql("TestFirmata");
-    });
-
-    it('sets @board to an empty string by default', function() {
-      expect(firmata.board).to.be.eql("");
+    it("sets @i2cReady to false by default", function() {
+      expect(firmata.i2cReady).to.be.eql(false);
     });
   });
 
-  describe('#commands', function() {
-    it('returns an array of Firmata method names', function() {
-      expect(firmata.commands()).to.be.a('array');
+  describe("#commands", function() {
+    it("returns an array of Firmata command names", function() {
+      var commands = firmata.commands();
+
+      expect(commands).to.be.a('array');
+
+      for (var i = 0; i < commands.length; i++) {
+        expect(commands[i]).to.be.a('string');
+      }
     });
   });
 
-  describe('#gpio', function() {
-    it('digitalRead', function() {
-      var board = {
-        MODES: { INPUT: 0 },
+  describe("#connect", function() {
+    var callback;
+
+    beforeEach(function() {
+      callback = spy();
+
+      stub(LibFirmata, 'Board').callsArg(1);
+      stub(firmata, 'proxyMethods');
+
+      firmata.connect(callback);
+    });
+
+    afterEach(function() {
+      LibFirmata.Board.restore();
+      firmata.proxyMethods.restore();
+    });
+
+    it("sets @board to a new LibFirmata.Board instance", function() {
+      expect(LibFirmata.Board).to.be.calledWithNew;
+      expect(firmata.board).to.be.eql({});
+    });
+
+    it("runs the callback when the board is connected", function() {
+      expect(callback).to.be.called;
+    });
+
+    it("emits 'connect' when the board is connected", function() {
+      expect(firmata.connection.emit).to.be.calledWith('connect');
+    });
+
+    it("proxies methods from the board to the adaptor instance", function() {
+      expect(firmata.proxyMethods).to.be.calledWith(
+        firmata.commands(),
+        firmata.board,
+        firmata
+      );
+    });
+  });
+
+  describe("#disconnect", function() {
+    beforeEach(function() {
+      firmata.board = { reset: spy() };
+
+      stub(Logger, 'debug');
+
+      firmata.disconnect();
+    });
+
+    afterEach(function() {
+      Logger.debug.restore();
+    });
+
+    it("logs that it's disconnecting from the board", function() {
+      expect(Logger.debug).to.be.calledWith("Disconnecting from board 'Firmata'")
+    });
+
+    it("calls #disconnect on the board", function() {
+      expect(firmata.board.reset).to.be.called;
+    });
+  });
+
+  describe("#digitalRead", function() {
+    var callback;
+
+    beforeEach(function() {
+      callback = spy();
+
+      firmata.board = {
         pinMode: spy(),
-        digitalRead: spy()
+        digitalRead: spy(),
+        MODES: { INPUT: 'r' }
       };
 
-      firmata.board = board;
-      firmata.digitalRead(3, function() {});
-
-      assert(board.pinMode.calledOnce);
-      assert(board.pinMode.calledWith(3, 0));
-      assert(board.digitalRead.calledOnce);
-      assert(board.digitalRead.calledWith(3));
+      firmata.digitalRead(4, callback);
     });
 
-    it('digitalWrite', function() {
-      var board;
-      board = {
-        MODES: {
-          OUTPUT: 1
-        },
+    it("sets the pin mode on the board to input", function() {
+      expect(firmata.board.pinMode).to.be.calledWith(4, 'r');
+    });
+
+    it("uses #digitalRead to get the value from the pin", function() {
+      expect(firmata.board.digitalRead).to.be.calledWith(4, callback);
+    });
+  });
+
+  describe("#digitalWrite", function() {
+    beforeEach(function() {
+      firmata.board = {
         pinMode: spy(),
-        digitalWrite: spy()
-      };
-      firmata.board = board;
-      firmata.digitalWrite(3, 1);
-      assert(board.pinMode.calledOnce);
-      assert(board.pinMode.calledWith(3, 1));
-      assert(board.digitalWrite.calledOnce);
-      assert(board.digitalWrite.calledWith(3, 1));
-    });
-
-    it('analogRead', function() {
-      var board = { analogRead: spy() };
-      var callback = function() {};
-
-      firmata.board = board;
-      firmata.analogRead(3, callback);
-
-      assert(board.analogRead.calledOnce);
-      assert(board.analogRead.calledWith(3, callback));
-    });
-
-    it('analogWrite', function() {
-      var board = {
-        MODES: { ANALOG: 1 },
-        pinMode: spy(),
-        analogPins: { 3: 'pin' },
-        analogWrite: spy()
+        digitalWrite: spy(),
+        MODES: { OUTPUT: 'w' }
       };
 
-      firmata.board = board;
-      firmata.analogWrite(3, 1);
-
-      assert(board.pinMode.calledOnce);
-      assert(board.pinMode.calledWith('pin', 1));
-      assert(board.analogWrite.calledOnce);
-      assert(board.analogWrite.calledWith('pin', 1));
+      firmata.digitalWrite(4, 1);
     });
 
-    it('pwmWrite', function() {
-      var board = {
-        MODES: { PWM: 'PWM' },
+    it("sets the pin mode on the board to output", function() {
+      expect(firmata.board.pinMode).to.be.calledWith(4, 'w');
+    });
+
+    it("uses #digitalWrite to write the value to the pin", function() {
+      expect(firmata.board.digitalWrite).to.be.calledWith(4, 1);
+    });
+  });
+
+  describe("#analogRead", function() {
+    var callback;
+
+    beforeEach(function() {
+      callback = spy();
+      firmata.board = { analogRead: spy() };
+      firmata.analogRead(4, callback);
+    });
+
+    it("uses #analogRead to get the pin value", function() {
+      expect(firmata.board.analogRead).to.be.calledWith(4, callback);
+    });
+  });
+
+  describe("#analogWrite", function() {
+    beforeEach(function() {
+      firmata.board = {
+        pinMode: spy(),
         analogWrite: spy(),
-        pinMode: spy()
+        analogPins: { 'a4': 4 },
+        MODES: { ANALOG: "a" }
       };
 
-      firmata.board = board;
-      firmata.pwmWrite(3, 'hello');
-
-      assert(board.pinMode.calledOnce);
-      assert(board.pinMode.calledWith(3, 'PWM'));
-      assert(board.analogWrite.calledOnce);
-      assert(board.analogWrite.calledWith(3, 'hello'));
+      firmata.analogWrite('a4', 0.5);
     });
 
-    it('servoWrite', function() {
-      var board = {
-        MODES: { SERVO: 'servo' },
-        analogWrite: spy(),
-        pinMode: spy()
-      };
+    it("sets the pin mode on the board to analog", function() {
+      expect(firmata.board.pinMode).to.be.calledWith(4, 'a');
+    });
 
-      firmata.board = board;
-      firmata.servoWrite(3, 'hello');
-
-      assert(board.pinMode.calledOnce);
-      assert(board.pinMode.calledWith(3, 'servo'));
-      assert(board.analogWrite.calledOnce);
-      assert(board.analogWrite.calledWith(3, 'hello'));
+    it("uses #analogWrite to write the value to the pin", function() {
+      expect(firmata.board.analogWrite).to.be.calledWith(4, 0.5);
     });
   });
 
-  describe('#i2c', function() {
-    it('i2cRead', function() {
-      var i2cconfig = firmata._i2cConfig = spy();
-      var board = { sendI2CReadRequest: spy(), sendI2CWriteRequest: spy() };
-      var callback = sinon.spy();
+  describe("#pwmWrite", function() {
+    beforeEach(function() {
+      firmata.board = {
+        pinMode: spy(),
+        analogWrite: spy(),
+        MODES: { PWM: 'p' }
+      };
 
-      firmata.i2cReady = false;
-      firmata.board = board;
-      firmata.i2cRead('address', 'command', 10, callback);
-
-      assert(i2cconfig.calledOnce);
-      assert(board.sendI2CWriteRequest.calledOnce);
-      assert(board.sendI2CWriteRequest.calledWith('address', ['command']));
-      assert(board.sendI2CReadRequest.calledOnce);
-      // TODO: Add assert for callback to make sure it is being called
-      // inside the anonymus function being passed as a callback to `board.sendI2CReadRequest`
-      //assert(callback.calledOnce);
+      firmata.pwmWrite(4 , 0.5);
     });
 
-    it('i2cWrite', function() {
-      var i2cconfig = firmata._i2cConfig = spy();
-      var board = { sendI2CWriteRequest: spy() };
+    it("sets the pin mode on the board to pwm", function() {
+      expect(firmata.board.pinMode).to.be.calledWith(4, 'p');
+    });
 
+    it("uses #analogWrite to write the value to the pin", function() {
+      expect(firmata.board.analogWrite).to.be.calledWith(4, 0.5);
+    });
+  });
+
+  describe("#servoWrite", function() {
+    beforeEach(function() {
+      firmata.board = {
+        pinMode: spy(),
+        analogWrite: spy(),
+        MODES: { SERVO: 's' }
+      };
+
+      firmata.servoWrite(4 , 0.5);
+    });
+
+    it("sets the pin mode on the board to servo", function() {
+      expect(firmata.board.pinMode).to.be.calledWith(4, 's');
+    });
+
+    it("uses #analogWrite to write the value to the pin", function() {
+      expect(firmata.board.analogWrite).to.be.calledWith(4, 0.5);
+    });
+  });
+
+  describe("#i2cWrite", function() {
+    var callback;
+
+    beforeEach(function() {
+      callback = spy();
+
+      firmata.board = { sendI2CWriteRequest: spy() };
+
+      stub(firmata, '_i2cConfig');
+    });
+
+    afterEach(function() {
+      firmata._i2cConfig.restore();
+    });
+
+    context("when the board is configured up for i2c", function() {
+      beforeEach(function() {
+        firmata.i2cReady = true;
+        firmata.i2cWrite('address', 'command', 'buffer', callback);
+      });
+
+      it("doesn't call #_i2cConfig", function() {
+        expect(firmata._i2cConfig).to.not.be.called;
+      });
+
+      it("sends a write request with #sendI2CWriteRequest", function() {
+        expect(firmata.board.sendI2CWriteRequest).to.be.calledWith(
+          'address',
+          ['command', 'buffer']
+        );
+      });
+
+      it("triggers the callback", function() {
+        expect(callback).to.be.called;
+      });
+    });
+
+    context("when the board is not configured up for i2c", function() {
+      beforeEach(function() {
+        firmata.i2cReady = false;
+        firmata.i2cWrite('address', 'command', 'buffer', callback)
+      });
+
+      it("calls #_i2cConfig", function() {
+        expect(firmata._i2cConfig).to.be.called;
+      });
+    });
+  });
+
+  describe("#i2cRead", function() {
+    var callback;
+    beforeEach(function() {
+      callback = spy();
+
+      firmata.board = {
+        sendI2CWriteRequest: spy(),
+        sendI2CReadRequest: stub()
+      };
+
+      stub(firmata, '_i2cConfig');
+    });
+
+    afterEach(function() {
+      firmata._i2cConfig.restore();
+    });
+
+    context("When the board is configured for i2c", function() {
+      beforeEach(function() {
+        firmata.i2cReady = true;
+        firmata.i2cRead('address', 'command', 10, callback);
+      });
+
+      it("calls #sendI2CWriteRequest", function() {
+        expect(firmata.board.sendI2CWriteRequest).to.be.called;
+      });
+
+      it("calls #sendI2CReadRequest", function() {
+        expect(firmata.board.sendI2CReadRequest).to.be.called;
+      });
+
+      context("when an error occurs", function() {
+        beforeEach(function() {
+          firmata.board.sendI2CReadRequest.callsArgWith(2, { name: 'Data' });
+          firmata.i2cRead('address', 'command', 10, callback);
+        });
+
+        it("calls the callback with an error and no data", function() {
+          expect(callback).to.be.calledWith(null, { name: 'Data' });
+        });
+      });
+
+      context("when data is returned", function() {
+        beforeEach(function() {
+          firmata.board.sendI2CReadRequest.callsArgWith(2, { name: 'Error' });
+          firmata.i2cRead('address', 'command', 10, callback);
+        });
+
+        it("calls the callback with no error, and the data", function() {
+          expect(callback).to.be.calledWith({ name: 'Error' }, null);
+        });
+      });
+    });
+
+    context("When the board is not configured for i2c", function() {
+      beforeEach(function() {
+        firmata.i2cReady = false;
+        firmata.i2cRead('address', 'command', 10, callback)
+      });
+
+      it("calls #_i2cConfig, telling it to delay for 2000ms", function() {
+        expect(firmata._i2cConfig).to.be.calledWith(2000);
+      });
+    })
+  });
+
+  describe("#_i2cConfig", function() {
+    beforeEach(function() {
+      firmata.board = { sendI2CConfig: spy() };
       firmata.i2cReady = false;
-      firmata.board = board;
-      firmata.i2cWrite('address', 'command', ['buffer']);
+      firmata._i2cConfig(2000);
+    });
 
-      assert(i2cconfig.calledOnce);
-      assert(board.sendI2CWriteRequest.calledOnce);
-      assert(board.sendI2CWriteRequest.calledWith('address', ['command', 'buffer']));
+    it("calls #sendI2CConfig on the board with the provided delay", function() {
+      expect(firmata.board.sendI2CConfig).to.be.calledWith(2000);
+    });
+
+    it("sets @i2cReady to true", function() {
+      expect(firmata.i2cReady).to.be.eql(true);
     });
   });
 });
