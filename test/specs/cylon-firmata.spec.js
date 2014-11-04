bded79abbabbc0caced1286c6195cb82026b166a
Andrew Stewart <andrew@stwrt.ca>
2014-11-04 11:03:32 -0800

    Update module registration for latest Cylon changes

--- test/specs/cylon-firmata.spec.js
+++ test/specs/cylon-firmata.spec.js
@@ -1,39 +1,19 @@
 'use strict';
 
-var GPIO = require('cylon-gpio'),
-    I2C = require('cylon-i2c');
-
 var Adaptor = source('firmata');
 
 var module = source('cylon-firmata');
 
 describe('cylon-firmata', function() {
-  describe('#register', function() {
-    var bot;
-
-    before(function() {
-      bot = { registerAdaptor: spy() };
-      stub(GPIO, 'register');
-      stub(I2C, 'register');
-
-      module.register(bot);
+  describe("#adaptors", function() {
+    it('is an array of supplied adaptors', function() {
+      expect(module.adaptors).to.be.eql(['firmata']);
     });
+  });
 
-    after(function() {
-      GPIO.register.restore();
-      I2C.register.restore();
-    });
-
-    it("registers 'firmata' with the passed Robot", function() {
-      expect(bot.registerAdaptor).to.be.calledWith('cylon-firmata', 'firmata');
-    });
-
-    it('registers the robot with the cylon-gpio module', function() {
-      expect(GPIO.register).to.be.calledWith(bot);
-    });
-
-    it('registers the robot with the cylon-i2c module', function() {
-      expect(I2C.register).to.be.calledWith(bot);
+  describe("#dependencies", function() {
+    it('is an array of supplied dependencies', function() {
+      expect(module.dependencies).to.be.eql(['cylon-gpio', 'cylon-i2c']);
     });
   });
 
